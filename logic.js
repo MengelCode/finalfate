471468e42d552712d2cecf3cae9ffe820dde7438
Manuel Engel <manuel-engel-97@gmx.de>
2019-11-10 21:23:21 +0100

    Added H letter to health power-up.

--- logic.js
+++ logic.js
@@ -1733,6 +1733,14 @@ function healthBoost_render() {
     //Num pad on mobile.
     context.fillStyle = "green";
     simpleSquare_render.call(this);
+    context.fillStyle = "white";
+    //H
+    //Left line
+    context.fillRect((this.middleX * 10)-10, (this.middleY *10)-6,7,24);
+    //Middle line
+    context.fillRect((this.middleX * 10)-10, (this.middleY *10)+5,24,7);
+    //Right line
+    context.fillRect((this.middleX * 10)+8, (this.middleY *10)-6,7,24);
 }
 
 
