25f1fb05f2d0e8fa9c924079859c6b07f6254f26
Manuel Engel <manuel-engel-97@gmx.de>
2020-09-06 01:55:37 +0200

    Continue to modularize JS code --> mostly only entities and input related things missing.

---
+++ finalfate/gameScreens/saveLoadError.js
@@ -0,0 +1,75 @@
+/**
+ * saveLoadError.js
+ * Contains the rendering cycles the "save game corrupted" and "could not
+ * access storage" errors and additional data used for it.
+ */
+
+var info_string = "Data Management";
+var data_corrupt = ["Saved game corrupted.", "Deletion required.", "Press SPACE on keyboard or any", "key on gamepad to continue."];
+var data_load_fail = ["Cannot access local storage.","Either your browser is too old or you disabled","web storage / cookies for this website.","Progress cannot be saved."];
+var data_warning_ok = "Confirm";
+/**
+ * Confirmation prompt 
+ * @returns {undefined}
+ */
+function loadFail() {
+    title_and_copyright_render();
+    validateReleasedState();
+    context.fillStyle = "blue";
+    context.fillRect(255, 190, 260, 200);
+    context.font = "27px Nonserif";
+    context.fillStyle = "white";
+    context.fillText(info_string, 255, 220);
+    context.font = "14px Nonserif";
+    context.fillText(data_load_fail[0], 255, 250);
+    context.fillText(data_load_fail[1], 255, 275);
+    context.fillText(data_load_fail[2], 255, 290);
+    context.fillText(data_load_fail[3], 255, 305);
+    if (aniCount === 3) {
+        initAllInput();
+    }
+    if (shoot && shootReleased) {
+        simplyPlaySound(sfx4);
+        exchangeRenderLoop(skillPrompt);
+        shootReleased = false;
+    }
+    if (aniCount % 5 === aniCount % 10) {
+        context.font = "27px Nonserif";
+        context.fillStyle = "yellow";
+        context.fillText(data_warning_ok, 340, 380);
+    }
+}
+
+
+/**
+ * Should never be happening. But it can, especially if an explorer plays with the value on their own.
+ * @returns {undefined}
+ */
+function saveCorrupt() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 800, 600);
+    context.fillStyle = "red";
+    context.fillRect(290, 190, 260, 200);
+    context.font = "27px Nonserif";
+    context.fillStyle = "white";
+    context.fillText(info_string, 290, 220);
+    context.font = "14px Nonserif";
+    context.fillText(data_corrupt[0], 290, 250);
+    context.fillText(data_corrupt[1], 290, 275);
+    context.fillText(data_corrupt[2], 290, 290);
+    context.fillText(data_corrupt[3], 290, 305);
+    if (aniCount === 3) {
+        initAllInput();
+    }
+    if (keyboard || gamepad !== false) {
+        simplyPlaySound(sfx4);
+        getLocalStorage().removeItem(gameStorageName);
+        initAllInput();
+        exchangeRenderLoop(titleScreen);
+    }
+    if (aniCount % 5 === aniCount % 10) {
+        context.font = "27px Nonserif";
+        context.fillStyle = "yellow";
+        context.fillText(data_warning_ok, 360, 380);
+    }
+}
--- finalfate/misc.js
+++ finalfate/misc.js
@@ -1,7 +1,7 @@
 /**
  * misc.js
  * Contains game-relevant functions which do not belong to another place 
- * in the code.
+ * in the code or data / code which is multi-purpose.
  */
 /**
  * (Re-)Play sound with just one call instead of three.
@@ -19,3 +19,35 @@ function simplyPlaySound(soundObject) {
  * @returns {undefined}
  */
 function func_noOp() {}
+
+
+/**
+ * Exchange the rendering loop with another function.
+ * Takes care of necessary cleanup and resets frame counter.
+ * @param {function} func Function which will be executed as rendering loop.
+ * @param {boolean} preserveCounters Do not reset counters. Default: false
+ * @returns {undefined}
+ */
+function exchangeRenderLoop(func, preserveCounters = false) {
+    if (renderTimer !== null)
+        clearInterval(renderTimer);
+    if (!preserveCounters) {
+        aniCount = renderReset;
+        aniCountRelative = 0;
+    }
+    testStorageState();
+    if (storageStatus === "CORRUPT") {
+        renderFunction = saveCorrupt;
+    } else {
+        renderFunction = func;
+    }
+    renderTimer = setInterval(renderFunction, FRAME_RATE);
+}
+
+//General-purpose dialogue options and text.
+var youSure = ["No", "Yes"];
+var youSureQuestion = ["Are you sure?"];
+
+// General variables for the game.
+//CHEAT ZONE - default 30. Frame rate of game.
+var FRAME_RATE = 30;
