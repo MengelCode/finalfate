0744b20807df4931967abd627d8a280f39714338
Manuel Engel <manuel-engel-97@gmx.de>
2023-06-25 01:40:42 +0200

    issue-122-bad-ending: Actual bad ending done.

--- finalfate/gameScreens/badEnding.js
+++ finalfate/gameScreens/badEnding.js
@@ -6,6 +6,10 @@
 
 
 var badEndingScene_moon = null;
+var badEndingScene_text = new Array();
+badEndingScene_text[0] = "Haha, these fools! They thought they got us.";
+badEndingScene_text[1] = "But their surprise is yet to come. Universe will";
+badEndingScene_text[2] = "belong to us, once and forevermore...";
 
 function badEndingScene() {
     clearScreen();
@@ -17,4 +21,15 @@ function badEndingScene() {
     context.font = "26px sans-serif";
     context.fillStyle = "gold";
     context.fillText("YOUR SCORE: " + player.score,180,560);
+    var text_x = 275;
+    var text_y = 450;
+    context.font = "12px sans-serif";
+    context.fillStyle = "crimson";
+    context.fillText(badEndingScene_text[0],text_x,text_y);
+    context.fillText(badEndingScene_text[1],text_x,text_y + 20);
+    context.fillText(badEndingScene_text[2],text_x,text_y + 40);
+    if(aniCount>400){
+        exchangeRenderLoop(gameOver);
+    }
+    
 }
