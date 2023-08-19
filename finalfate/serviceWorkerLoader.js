1e7ef1733af2c02e6a00712e19d3de392383b5d1
Manuel Engel <manuel-engel-97@gmx.de>
2023-08-19 15:01:57 +0200

    issue-142-pwa: Fix even more smaller issues with the PWA files

--- finalfate/serviceWorkerLoader.js
+++ finalfate/serviceWorkerLoader.js
@@ -7,9 +7,6 @@
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+    navigator.serviceWorker.register("serviceWorker.js");
   });
 }
