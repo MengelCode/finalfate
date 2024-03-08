5e562f0b2c5ff673a1a39264321061b352121497
Manuel Engel <manuel-engel-97@gmx.de>
2024-03-08 22:49:28 +0100

    issue-91-level-5 Make boss appearance worse, but improve performance and placement.

--- finalfate/entities/combinedEntities/bosses/boss5.js
+++ finalfate/entities/combinedEntities/bosses/boss5.js
@@ -11,6 +11,8 @@ class MetallicMoon extends Enemy {
         super(middleX,middleY, func_noDim, metallicMoon_update, metallicMoon_render);
         this.middleX = middleX*10;
         this.middleY = middleY*10;
+        this.firstUpdate = true;
+        this.trueEnemy = true;
         
     }
     
@@ -18,9 +20,20 @@ class MetallicMoon extends Enemy {
 }
 
 function metallicMoon_update(){
-    giant_boss = this;
+    if(this.firstUpdate){
+        giant_boss = this;
+        this.firstUpdate = false;
+    }
+    
 }
 
 function metallicMoon_render(){
+    var originalX = this.middleX;
+    var originalY = this.middleY;
+    this.middleX = 250;
+    this.middleY = 250;
     meMoOut_render.call(this);
+    this.middleX = originalX;
+    this.middleY = originalY;
+
 }
--- finalfate/entities/decorative/MetallicMoonOuterior.js
+++ finalfate/entities/decorative/MetallicMoonOuterior.js
@@ -3,12 +3,12 @@
  */
 
 class MetallicMoonOuterior extends Decoration {
-    
-    constructor(posX,posY,updateFctNo){
-        super(posX,posY,meMoOut_render,meMoOut_update[updateFctNo]);
+
+    constructor(posX, posY, updateFctNo) {
+        super(posX, posY, meMoOut_render, meMoOut_update[updateFctNo]);
         this.posZ = 0;
     }
-    
+
 }
 
 var meMoOut_update = new Array(3);
@@ -16,31 +16,32 @@ meMoOut_update[0] = func_noOp;
 
 
 
-function meMoOut_render(){
+function meMoOut_render() {
     //Body of Metallic Moon.
     //Head.
     context.fillStyle = "gray";
-    context.arc(this.middleX,this.middleY,160,0,2 * Math.PI);
-    context.fill();
+    //context.arc(this.middleX, this.middleY, 160, 0, 2 * Math.PI);
+    //context.fill();
+    context.fillRect(this.middleX-150,this.middleY-150,300,300);
     //Eyes.
     context.fillStyle = "red";
-    context.fillRect(this.middleX-65,this.middleY-65,40,40);
-    context.fillRect(this.middleX+35,this.middleY-65,40,40);
+    context.fillRect(this.middleX - 65, this.middleY - 65, 40, 40);
+    context.fillRect(this.middleX + 35, this.middleY - 65, 40, 40);
     //Nose.
-    context.fillRect(this.middleX-10, this.middleY+10,40,40);
+    context.fillRect(this.middleX - 10, this.middleY + 10, 40, 40);
     //Mouth.
     var whichWay = aniCount % 60 > 30;
-    for(var i = -120; i<80; i+=40){
-        if (whichWay && i % 80 === 0) {
-            context.fillStyle = "red";
-        } else if (!whichWay && i % 80 === 0) {
-            context.fillStyle = "yellow";
-        } else if (whichWay) {
-            context.fillStyle = "yellow";
-        } else {
-            context.fillStyle = "red";
-        } 
-        context.fillRect(this.middleX+i+20,this.middleY+80,40,40);    
-    }
+        for (var i = -120; i < 80; i += 40) {
+            if (whichWay && i % 80 === 0) {
+                context.fillStyle = "red";
+            } else if (!whichWay && i % 80 === 0) {
+                context.fillStyle = "yellow";
+            } else if (whichWay) {
+                context.fillStyle = "yellow";
+            } else {
+                context.fillStyle = "red";
+            }
+            context.fillRect(this.middleX + i + 20, this.middleY + 80, 40, 40);
+        }
     
 }
