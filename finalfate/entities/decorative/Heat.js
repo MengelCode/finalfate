3dd7e325792a2a2a6c6a12b953125c06e6ef3600
Manuel Engel <manuel-engel-97@gmx.de>
2024-10-03 23:38:49 +0200

    issue-191 Remove flame stuff

--- finalfate/entities/decorative/Heat.js
+++ finalfate/entities/decorative/Heat.js
@@ -19,38 +19,5 @@ class Heat extends Decoration {
 function heat_render() {
     context.fillStyle = "#550000";
     context.fillRect(0, 0, 800, 600);
-
-    //Additional code while "heat" is active.
-
-    //Starting with relevant coordinates and data. 
-    player_x_real = transformToCanvasCoords(player.middleX);
-    player_y_real = transformToCanvasCoords(player.middleY);
-    //Positions general array.
-    flame_positions = [];
-    //Array for first part of loop.
-    flame_positions[0] = [];
-    flame_positions[0][0] = [player_x_real - 22, player_y_real - 18,"#dd0000"];
-    flame_positions[0][1] = [player_x_real - 17, player_y_real - 16,"#dd0000"];
-
-
-
-    flame_size = 8;
-
-    //Different displays of "flames".
-    if(aniCount%3 == 0){
-        for(var i = 0; i<flame_positions[0].length; i++){
-            context.fillStyle = flame_positions[0][i][2];
-            context.fillRect(flame_positions[0][i][0], flame_positions[0][i][1],flame_size,flame_size);       
-        }
-        
-    }
-
-    else if(aniCount%3 == 1){
- 
-    }
-
-    else {
-
-    }
 }
 
