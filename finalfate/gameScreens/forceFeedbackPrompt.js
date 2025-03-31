d9460850e0136c64748ed051978bec30faf65d6d
Manuel Engel <manuel-engel-97@gmx.de>
2025-03-31 23:42:25 +0200

    issue-195 Finished force feedback selection menu

--- finalfate/gameScreens/forceFeedbackPrompt.js
+++ finalfate/gameScreens/forceFeedbackPrompt.js
@@ -3,7 +3,8 @@
  * Prompt to decide whether to attempt to enable force feedback or not.
  */
 
-var fefoPrompt_JumpTo = null;
+var fefoPrompt_jumpTo = null;
+var fefoPrompt_decision = null;
 const FEFO_PROMPT_TITLE = "Rumble Function";
 const FEFO_PROMPT_DETECTED = "Rumble / Vibration support has been detected.";
 const FEFO_PROMPT_GENERAL_TEXT = "Do you want to enable this function";
@@ -12,7 +13,12 @@ const FEFO_PROMPT_GAMEPAD = "of your gamepad?";
 
 //TODO Finish screen function.
 function forceFeedbackPrompt(){
-
+    if(fefoPrompt_jumpTo === null){
+        throw new Error("Screen has not been setup properly!");
+    }
+    if(fefoPrompt_decision === null){
+        fefoPrompt_decision = true;
+    }
     title_and_copyright_render();
     title_animation_render();
     validateReleasedState();
@@ -21,7 +27,7 @@ function forceFeedbackPrompt(){
 
     context.fillStyle = "blue";
     context.fillRect(290, 190, 260, 200);
-
+    //Text.
     context.font = "27px sans-serif";
     context.fillStyle = "white";
     context.fillText(FEFO_PROMPT_TITLE, 290, 220);
@@ -32,7 +38,26 @@ function forceFeedbackPrompt(){
     context.fillStyle = "gold";
     context.fillText(FEFO_PROMPT_GENERAL_TEXT, 290, 275);
     context.fillText(touchs ? FEFO_PROMPT_PHONE : FEFO_PROMPT_GAMEPAD, 290, 290);
+    //Yes / no elements.
+    context.fillStyle = fefoPrompt_decision ? "gold" : "white";
+    context.fillText(youSure[1], 310, 380);
+    context.fillStyle = !fefoPrompt_decision ? "gold" : "white";
+    context.fillText(youSure[0], 350, 380);
 
-
+    //Selecting around.
+    if (right && axisXReleased && fefoPrompt_decision) {
+        simplyPlaySound(sfx4);
+        fefoPrompt_decision = false;
+        axisXReleased = false;
+    } else if (left && axisXReleased && !fefoPrompt_decision) {
+        simplyPlaySound(sfx4);
+        fefoPrompt_decision = true;
+        axisXReleased = false;
+    }
+    if (shoot && shootReleased) {
+        simplyPlaySound(sfx4);
+        exchangeRenderLoop(fefoPrompt_jumpTo);
+        shootReleased = false;
+    }
 
 }
